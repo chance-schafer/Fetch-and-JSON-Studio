b4a015783bbca854f7dcb10c1bab14963bb71971
Chance Schafer <chance.schafer.dev@gmail.com>
2022-08-11 23:31:16 -0500

    completed bonus missions

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 window.addEventListener('load', () => {
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then((res) => {
         res.json().then( (json) => {
+            json = json.sort((a, b) => {
+                return b.hoursInSpace - a.hoursInSpace
+            })
+            console.log(json)
             const div = document.getElementById('container')
+            div.innerHTML += `<h2>Total Astronauts: ${json.length}`
             for(let i = 0; i < json.length; i++) {
                 let skills = json[i].skills
                 div.innerHTML += `
@@ -11,7 +16,7 @@ window.addEventListener('load', () => {
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li id="active">Active: ${json[i].active}</li>
+                                <li class="${json[i].active}">Active: ${json[i].active}</li>
                                 <li>Skills: ${json[i].skills.join(', ')}</li>
                             </ul>
                         </div>
